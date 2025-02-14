f948f14a29ec01cc2523a1b3fb26546697301af7
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-02-14 19:30:56 +0530

    C++ in progress

---
+++ 03_Loops/18_Output.cpp
@@ -0,0 +1,35 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 1;
+    while( x == 1)
+    x -= 1;
+    cout << x << endl;
+}                                    // Output = 0
+
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    while (i = 10)
+    {
+        cout << i << endl;
+        i = i + 1;                           // Infinite loop  10
+    }
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    while ('a' < 'b')                                   // ASCII Values (97 < 98)
+    cout << "malayalam is a palindrome" << endl;
+}                                                         // Infinite Loop
+*/
+
+//4.36.36
